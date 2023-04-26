94f0b1b0cf50fc0721fc36fa0ec637469bbfd07a
Samuel <samuelfras@gmail.com>
2023-04-26 17:19:00 -0600

    took me a while, but done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(driver,query){
+   return  driver.filter((el) => el.toLowerCase().includes(query.toLowerCase()))
+}
+function fuzzyMatch(driver,query){
+return driver.filter(function(name) {
+   return name.startsWith(query);
+ });
+}
+
+// function fuzzyMatch(driver,query){
+//     return  driver.filter(function (driver) {
+//       console.log(driver.toLowerCase().substring(0 , driver.lenght) === query.toLowerCase()
+//     )}
+//     )
+//    }
+
+function matchName (driver, query){
+   return driver.filter(function (driver){
+      return driver.name === query
+   })
+}
